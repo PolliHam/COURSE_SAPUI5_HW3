0a68fbefce9722d9ec6b58872a7b0d49c5b0fbbc
Polina Shenets <polina.shenets@leverx.com>
2019-10-02 13:39:33 +0000

    Homework 3 v1.0

--- webapp/controller/Table.controller.js
+++ webapp/controller/Table.controller.js
@@ -13,47 +13,12 @@ sap.ui.define([
 		 * @memberOf ZSHN.COURSE_SAPUI5.view.Table
 		 */
 		onInit: function () {
-			//var data = {};
-			var OModel = new JSONModel();
-			var xhr = new XMLHttpRequest();
-			xhr.addEventListener("readystatechange", function () {
-				if (this.readyState === 4) {
-					var result=JSON.parse(this.response);
-					//data.accounts=result;
-            	    //console.log(data);
-            	    OModel.setData(result);
-				}
-			});
-			xhr.open("GET", "http://localhost:2403/account");
-			xhr.setRequestHeader("Content-Type", "application/json");
-			xhr.send();
-			this.getView().setModel(OModel, 'accounts');
+			
+		},
+		onNavigate: function (oEvent) {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("Inputs");
 		}
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf ZSHN.COURSE_SAPUI5.view.Table
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf ZSHN.COURSE_SAPUI5.view.Table
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf ZSHN.COURSE_SAPUI5.view.Table
-		 */
-		//	onExit: function() {
-		//
-		//	}
 
 	});
 
